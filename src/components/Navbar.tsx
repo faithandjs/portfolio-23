2dade942f108409feb9d78a7feb8af192bb814e4
faithandjs <faiththe1rst@gmail.com>
2024-02-20 15:29:24 +0100

    clearing local

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Navbar() {
+  const router = useRouter();
+  const menu = ['about-me', 'projects', 'contact'];
+
+  const [data, setdata] = useState(menu);
+
+  // const [data, setdata] = useState(() => {
+  //   if (!menu.includes(router.pathname.substring(1))) return menu;
+
+  //   if (router.pathname.substring(1) === 'menu') return ['close'];
+
+  //   return ['menu'];
+  // });
+
+  useEffect(() => {
+    setdata(() => {
+      if (!menu.includes(router.pathname.substring(1))) return menu;
+
+      if (router.pathname.substring(1) === 'menu') return ['close'];
+
+      return ['menu'];
+    });
+  }, [router]);
   return (
     <nav className='flex justify-between text-xl py-9 px-7'>
       <h4>
         <Link href={'/'}>FAITH</Link>
       </h4>
       <ul className='flex gap-10'>
-        {['about-me', 'projects', 'contact'].map((item, key) => (
+        {data.map((item, key) => (
           <li key={key} className='after capitalize'>
             {item.split('-').join(' ')}
             <Link href={item} className='absolute inset-0 z-[4000]'></Link>
---
+++ src/pages/about-me.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <section>
+      <div>
+        <p>
+          Hi, I'm Faith, i’m a frontend developer from Abuja, Nigeria. I've
+          always been captivated by the art of bringing stunning web designs to
+          life as actual websites and this fascination led me to frontend
+          development.
+        </p>
+        <p>
+          In 2022, I started teaching myself to code. Although I had tried,
+          unsuccessfully, to learn in 2020, I found renewed enthusiasm when I
+          picked it up again. I learnt from anywhere I could, Twitter, youtube,
+          articles, websites books, Udemy, among others, and so far, I have
+          fallen in love with coding, I love learning and applying my knowledge
+          and seeing the result, it’s a very rewarding cycle.
+        </p>
+        <p>
+          With a degree in computer science, I have a solid foundation in
+          computer basics and acquired skills that I brings to my work. I am
+          always open to new challenges and opportunities to learn, grow and
+          expand my skillset.
+        </p>
+
+        <div>
+          In my leisure time, I love to read. My favorite genre is African
+          fiction but I’m trying to diversify, I also read Spanish and web
+          development books.
+          <p> My favorite books are: </p>
+          <ul>
+            <li>
+              His only wife <i>- Peace Adzo Medie</i>
+            </li>
+            <li>
+              Akata witch <i>- Nnedi Okarafor</i>
+            </li>
+            <li>
+              The death of Vivek Orii <i>- Akwaeke Emezi</i>
+            </li>
+            <li>
+              Under the Udala tree <i>- Chinelo Okparanta</i>
+            </li>
+            <li>
+              The hairdresser of Harare <i>- Tendai Uchu</i>
+            </li>
+          </ul>
+          <p>
+            I also like to watch movies and TV shows and hang out with friends
+            and family.
+          </p>
+        </div>
+        <p>
+          I am currently available for work and open to exploring new job
+          opportunities.
+        </p>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default About;
+
